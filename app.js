c3be44776b569feac273611e7281419cd7efd95c
saretotracy <tracysareto@gmail.com>
2021-12-10 00:29:25 +0300

    js functions

---
+++ app.js
@@ -0,0 +1,14 @@
+const generate_btn = document.querySelector("#generatebtn");
+const cancel_btn = document.querySelector(".cancel");
+const front = document.querySelector(".front");
+const next = document.querySelector(".next");
+
+
+generate_btn.addEventListener("click", () => {
+    front.classList.add("display")
+    next.classList.remove("display")
+});
+cancel_btn.addEventListener("click", () => {
+    front.classList.remove("display")
+    next.classList.add("display")
+});
