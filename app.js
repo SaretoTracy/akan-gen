0ee4f907ea74936530b54be2975f8acf554c40a6
saretotracy <tracysareto@gmail.com>
2021-12-10 23:36:07 +0300

    select form document

--- app.js
+++ app.js
@@ -16,4 +16,32 @@ cancel_btn.addEventListener("click", () => {
 const femaleNames = ["Akosua, Adwoa ,Abenaa, Akua, Yaa, Afua, Ama"];
 const maleNames = ["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"];
 const weekDays = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
-const months = ["January, Febuary , March, April, June, July, August, September, October, November, December"]
+const months = ["January, Febuary , March, April, June, July, August, September, October, November, December"];
+
+
+/**data collection */
+let birthYear = document.querySelector(".year");
+let birthMonth = document.querySelector(".month");
+let birthDate = document.querySelector(".date");
+let birthYear = document.querySelector("#gender");
+
+
+/**data collection input*/
+let dataInput = (year, month, date) => {
+    if (year === null) {
+        alert("Please enter year")
+    } else if (year <= 0 || year.length > 4) {
+        alert("Invalid Year")
+    }
+    if (month === null) {
+        alert("please enter month")
+    } else if (date <= 0 || date > 12) {
+        alert("Invalid Date")
+    }
+    if (date === null) {
+        alert("please enter date")
+    } else if (date <= 0 || date >= 32) {
+        alert("Invalid Date")
+    }
+
+}
