4fecb5eb85feea5468cb651a91df2f9689ff0894
saretotracy <tracysareto@gmail.com>
2021-12-11 10:11:01 +0300

    modification

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ cancel_btn.addEventListener("click", () => {
     next.classList.add("display")
 });
 
+/**Akan name js */
+
 const femaleNames = ["Akosua, Adwoa ,Abenaa, Akua, Yaa, Afua, Ama"];
 const maleNames = ["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"];
 const weekDays = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
@@ -23,25 +25,5 @@ const months = ["January, Febuary , March, April, June, July, August, September,
 let birthYear = document.querySelector(".year");
 let birthMonth = document.querySelector(".month");
 let birthDate = document.querySelector(".date");
-let birthYear = document.querySelector("#gender");
-
-
-/**data collection input*/
-let dataInput = (year, month, date) => {
-    if (year === null) {
-        alert("Please enter year")
-    } else if (year <= 0 || year.length > 4) {
-        alert("Invalid Year")
-    }
-    if (month === null) {
-        alert("please enter month")
-    } else if (date <= 0 || date > 12) {
-        alert("Invalid Date")
-    }
-    if (date === null) {
-        alert("please enter date")
-    } else if (date <= 0 || date >= 32) {
-        alert("Invalid Date")
-    }
-
-}
+let gender = document.querySelector("#gender");
+let generatebtn = document.querySelector("#generate_Btn")
