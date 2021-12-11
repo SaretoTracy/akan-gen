2f9b59b7c9e285142079914a1601bdbd39643341
saretotracy <tracysareto@gmail.com>
2021-12-11 13:33:55 +0300

    modification

--- app.js
+++ app.js
@@ -71,10 +71,9 @@ let dataInput = (birthYear, birthMonth, birthDate) => {
 
 
 /**
-generate formula (Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7) 
+generate formula (Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7) */
 
 let CC = (Math.floor(birthYear / 100));
-let YY = parseInt(birthYear.slice(-2));
+let YY = parseInt(birthYear.slice(2, 3));
 let MM = [parseInt(birthMonth) - 1];
 let DD = parseInt(birthDate);
-*/
