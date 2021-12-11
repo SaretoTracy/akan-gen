bb6a0222ad8cbb18484edd7c27f66913f35738f5
saretotracy <tracysareto@gmail.com>
2021-12-11 14:20:27 +0300

    add call functions

--- app.js
+++ app.js
@@ -26,54 +26,61 @@ let form = document.querySelector(".form")
 let birthYear = document.querySelector(".year");
 let birthMonth = document.querySelector(".month");
 let birthDate = document.querySelector(".date");
+let genderFemale = document.querySelector("#female");
+let genderMale = document.querySelector("#male");
 
 
 
 
 
 form.addEventListener("submit", function(e) {
-        e.preventDefault()
-        console.log(birthYear.value);
-        console.log(birthMonth.value);
-        console.log(birthDate.value);
-        console.log(dataInput.value);
+    e.preventDefault()
+    console.log(birthYear.value);
+    console.log(birthMonth.value);
+    console.log(birthDate.value);
 
+    validation();
+    generate();
 
-    })
-    /** 
+})
 
-    /**data collection input*/
-let dataInput = (birthYear, birthMonth, birthDate) => {
-    if (birthYear === null) {
+/**data collection input*/
+function validation() {
+    if (birthYear.value === null) {
         alert("Please enter Year")
-    } else if (birthYear <= 0 || birthYear.length > 4) {
+    } else if (birthYear.value <= 0 || birthYear.value.length > 4) {
         alert("Invalid Year")
     }
-    if (birthMonth === null) {
+    if (birthMonth.value === null) {
         alert("please enter Month")
-    } else if (birthMonth <= 0 || birthMonth > 12) {
+    } else if (birthMonth.value <= 0 || birthMonth.value > 12) {
         alert("Invalid Month")
     }
-    if (birthDate === null) {
+    if (birthDate.value === null) {
         alert("please enter birthDate")
-    } else if (birthDate <= 0 || birthDate >= 32) {
+    } else if (birthDate.value <= 0 || birthDate.value >= 32) {
         alert("Invalid Date")
     }
-    if (birthMonth === 2 && Number(birthYear) % 4 === 0) {
+    if (birthMonth.value === 2 && (birthYear.value) % 4 === 0) {
         if (birthDate > 28 || birthDate < 1) {
             alert("Invalid Date")
-        } else if (birthMonth === 2 && birthDate > 29) {
+        } else if (birthMonth.value === 2 && birthDate.value > 29) {
             alert("Invalid Date")
         }
     }
 }
 
 
-
 /**
 generate formula (Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7) */
+function generate() {
 
-let CC = (Math.floor(birthYear / 100));
-let YY = parseInt(birthYear.slice(2, 3));
-let MM = [parseInt(birthMonth) - 1];
-let DD = parseInt(birthDate);
+    let CC = (Math.floor(birthYear / 100));
+    console.log("born" + birthYear.value)
+    let YY = parseInt(birthYear.value.slice(2, 4));
+    console.log(YY);
+    let MM = [parseInt(birthMonth) - 1];
+    let DD = parseInt(birthDate);
+
+}
+Dayoftheweek(d) = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
